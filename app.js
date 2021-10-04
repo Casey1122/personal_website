c37c2380bdfcd57d4c84d324a9a4ffdc963ad50b
Casey1122 <caseyccs1122@gmail.com>
2021-10-04 15:52:48 +0800

    4_Oct

---
+++ app.js
@@ -0,0 +1,45 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`;
+            }
+        });
+
+        burger.classList.toggle('toggle');
+
+    });
+}
+
+navSlide();
+
+var form = document.getElementById("my-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    status.innerHTML = "Success";
+    status.classList.add('success');
+    form.reset()
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem";
+    status.classList.add('error');
+  });
+}
+form.addEventListener("submit", handleSubmit)
